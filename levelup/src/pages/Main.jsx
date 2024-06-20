da78492ba95982397a43e2d872b1189e0c1fdbd1
abhinav <abhinavrajesh2002@gmail.com>
2024-06-20 18:47:07 +0530

    some installation

---
+++ levelup/src/pages/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+        <h1 className='text-blue-500'>hello world</h1>
+    </div>
+  )
+}
+
+export default Main
---
+++ levelup/tailwind.config.js
@@ -0,0 +1,17 @@
+const flowbite = require("flowbite-react/tailwind");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx}",
+    "./node_modules/flowbite/**/*.js",
+    ...flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin'),
+    ...flowbite.plugin(),
+  ],
+};
