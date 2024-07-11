412f9e7826f9c61e9a6de90faf16e63751e54718
abhinav <abhinavrajesh2002@gmail.com>
2024-07-11 12:22:51 +0530

    small edit

--- levelup/src/components/Create_Team_Modale.jsx
+++ levelup/src/components/Create_Team_Modale.jsx
@@ -138,7 +138,7 @@ const Create_Team_Modale = ({ setmodale, options }) => {
                         name="brand"
                         id="brand"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="Product brand"
+                        placeholder=""
                         required
                       />
                     </div>
